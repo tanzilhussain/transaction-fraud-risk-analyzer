d77c502ce8f7e20b6bd179eadf0cd19409a57f9f
Tanzil Hussain <tanzilhannah@gmail.com>
2025-07-17 23:17:31 -0700

    added shap

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import streamlit as st
+from sklearn.preprocessing import LabelEncoder
 
-st.title("Financial Transaction Fraud Detection Dashboard")
+with open("style.css") as css:
+     st.markdown(f"<style>{css.read()}</style>", unsafe_allow_html=True)
+
+st.markdown("<h1 class='dashboard_title'>Financial Transaction Fraud Detection Dashboard</h1>", unsafe_allow_html=True)
+
 with st.form("simulation"):
     # transaction type
-    st.selectbox('Transaction Type: ', ('Transfer', 'Cashing Out', 'Cashing In', 'Debit', 'Payment'))
+    type = st.selectbox('Transaction Type: ', ('Transfer', 'Cashing Out', 'Cashing In', 'Debit', 'Payment'))
     st.number_input('Enter Transaction Amount: ')
     sender_curr_balance = st.number_input('Sender Account Balance (before transaction): ')
     receiver_curr_balance = st.number_input('Receiver Account Balance (before transaction): ')
     submitted = st.form_submit_button("Submit")
-    # if submitted:
+    if submitted:
+        # handling categorical data 
+        le = LabelEncoder()
+        encoded_type = le.transform(type)
     # ['TRANSFER', 'CASH_OUT', 'CASH_IN', 'DEBIT', 'PAYMENT']
